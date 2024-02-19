39d3b306300be6d4613bde4a582d03a1ed137d0e
zaltvapp <45437371+zaltvapp@users.noreply.github.com>
2024-02-18 22:48:56 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -22,27 +22,11 @@ print_banner() {
   printf "\n\n"
 
   printf "${GREEN}";
-  printf "                                                     ▄▄█▀▀▀▀▀▀▀█▄▄  \n";
-  printf "                                                   ${GREEN}▄█▀${NC}   ${WHITE}▄▄${NC}      ${GREEN}▀█▄\n";
-  printf "                                                   ${GREEN}█${NC}    ${WHITE}███${NC}         ${GREEN}█\n";
-  printf "                                                   ${GREEN}█${NC}    ${WHITE}██▄         ${GREEN}█${NC}\n";
-  printf "                                                   ${GREEN}█${NC}     ${WHITE}▀██▄${NC} ${WHITE}██${NC}    ${GREEN}█\n";
-  printf "                                                   ${GREEN}█${NC}       ${WHITE}▀███▀${NC}    ${GREEN}█\n";
-  printf "                                                   ${GREEN}▀█▄           ▄█▀\n";
-  printf "                                                    ▄█    ▄▄▄▄█▀▀  \n";
-  printf "                                                    █  ▄█▀        \n";
-  printf "                                                    ▀▀▀▀          \n";
   printf "${NC}";
 
   printf "\n"
 
 printf "${GREEN}";  
-printf "██████╗░██╗░░░░░░██╗░░░░░░░██╗\n";
-printf "██╔══██╗██║░░░░░░██║░░██╗░░██║\n";
-printf "██████╔╝██║░░░░░░╚██╗████╗██╔╝\n";
-printf "██╔═══╝░██║░░░░░░░████╔═████║░\n";
-printf "██║░░░░░███████╗░░╚██╔╝░╚██╔╝░\n";
-printf "╚═╝░░░░░╚══════╝░░░╚═╝░░░╚═╝░░\n";
 printf "${NC}";
   
   
